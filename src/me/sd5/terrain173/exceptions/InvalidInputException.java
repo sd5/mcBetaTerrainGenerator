462aa11e4214ea28dab51e5149693baaac3d5211
sd5 <timo.braendlin@googlemail.com>
2013-06-26 16:51:30 +0200

    Added the user input parser and an exception if the parsing process fails.

---
+++ src/me/sd5/terrain173/exceptions/InvalidInputException.java
@@ -0,0 +1,15 @@
+package me.sd5.terrain173.exceptions;
+
+/**
+ * @author sd5
+ * Throwed if there is an exception when trying to parse the user input.
+ */
+public class InvalidInputException extends Exception {
+	
+	public InvalidInputException(String message) {
+		
+		super(message);
+		
+	}
+	
+}
