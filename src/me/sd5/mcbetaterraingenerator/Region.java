d148156938f24087861156398c670c25f654ffaa
sd5 <timo.braendlin@googlemail.com>
2013-06-26 16:10:22 +0200

    Added Region.

--- src/me/sd5/mcbetaterraingenerator/Region.java
+++ src/me/sd5/mcbetaterraingenerator/Region.java
@@ -6,4 +6,38 @@ package me.sd5.mcbetaterraingenerator;
  */
 public class Region {
 
+	//The coordinates of this region.
+	private int x;
+	private int z;
+	
+	/**
+	 * Creates a new region.
+	 * @param x The X-coordinate of this region.
+	 * @param z The Z-coordinate of this region.
+	 */
+	public Region(int x, int z) {
+		
+		this.x = x;
+		this.z = z;
+		
+	}
+	
+	/**
+	 * @return The X-coordinate of this region.
+	 */
+	public int getX() {
+		
+		return x;
+		
+	}
+	
+	/**
+	 * @return The Z-coordinate of this region.
+	 */
+	public int getZ() {
+		
+		return z;
+		
+	}
+	
 }
