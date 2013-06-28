83e2af756c94b260b1dc7a228da12f46b15eca75
sd5 <timo.braendlin@googlemail.com>
2013-06-28 15:42:27 +0200

    Removed BetaServer and FinalServer because there is no difference between them by now.

--- src/me/sd5/mcbetaterraingenerator/BetaServer.java
+++
@@ -1,9 +0,0 @@
-package me.sd5.mcbetaterraingenerator;
-
-/**
- * @author sd5
- * A representation of a Beta 1.7.3 server.
- */
-public class BetaServer extends MinecraftServer {
-
-}
--- src/me/sd5/mcbetaterraingenerator/FinalServer.java
+++
@@ -1,9 +0,0 @@
-package me.sd5.mcbetaterraingenerator;
-
-/**
- * @author sd5
- * A representation of a Final 1.5.2 server.
- */
-public class FinalServer extends MinecraftServer {
-
-}
